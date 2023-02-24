e10a2158a0eaca959c0f98ab01192c9a35d6d855
Hermann EHO <hermannleboss@gmail.com>
2023-02-24 15:28:03 +0000

    feat: use off only to run single test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,4 +5,8 @@ describe("home page", () => {
         "Testing Next.js Applications with Cypress"
     )
   })
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
+  })
 })
