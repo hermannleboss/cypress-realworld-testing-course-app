8c46b5f2f5378280cad47c7b49798bf636c6dd45
Hermann EHO <hermannleboss@gmail.com>
2023-02-24 18:46:37 +0000

    feat: test invalid email address

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -9,10 +9,14 @@ describe("Newsletter Subscribe Form", () => {
     cy.getByData("submit-button").click()
     cy.getByData("success-message").should("exist").contains("tom@aol.com")
   })
-  it.only("does NOT allow an invalid email address", () => {
+  it("does NOT allow an invalid email address", () => {
     cy.getByData("email-input").type("tom")
     cy.getByData("submit-button").click()
     cy.getByData("success-message").should("not.exist")
   })
-
+  it.only("does NOT  allows users to subscribe 2 time into the email list", () => {
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message").should("exist").contains("Error: john@example.com already exists. Please use a different email address.")
+  })
 })
