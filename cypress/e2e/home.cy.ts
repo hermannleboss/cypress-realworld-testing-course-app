bbc883e1cd8b47d23c1d794fa47628c180104cd7
Hermann EHO <hermannleboss@gmail.com>
2023-02-24 15:29:22 +0000

    feat: practice test other feature

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -8,5 +8,7 @@ describe("home page", () => {
   it.only("the features on the homepage are correct", () => {
     cy.visit("http://localhost:3000")
     cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
 })
